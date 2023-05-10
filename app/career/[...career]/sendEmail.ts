638675115c87f5556409b68c49fea679904b6d48
JAYCEE <Jayceexxiii@gmail.com>
2023-05-09 23:13:13 -0700

    adding sendEmail

--- app/career/[...career]/sendEmail.ts
+++ app/career/[...career]/sendEmail.ts
@@ -4,7 +4,6 @@ import toast from "react-hot-toast";
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
 interface IJobApplication {
-<<<<<<< HEAD
     name: string;
     email: string;
     phone: string;
@@ -18,25 +17,10 @@ interface IJobApplication {
   export async function sendJobApplicationEmail({ name, email, coverLetter, jobpost_id}: IJobApplication): Promise<void> {
     try {
       const response = await axios.post('/api/sendEmail', {
-=======
-  name: string;
-  email: string;
-  phone: string;
-  coverLetter: string;
-  resume: File;
-}
-
-export async function sendJobApplicationEmail({
-  name,
-  email,
-  coverLetter,
-}: IJobApplication): Promise<void> {
-  try {
-    const response = await axios.post("/api/sendEmail", {
->>>>>>> 1017af4611744128b8eaa8e0e386cefee28c9d84
       name: name as string,
       email: email as string,
       message: coverLetter as string,
+      jobpost_id: jobpost_id as string,
     });
 
     toast.success("Application has been sumbitted");
