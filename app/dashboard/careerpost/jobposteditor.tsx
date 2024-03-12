b5f1311515079f1549c14957147413922b6e0f4a
ovswillingworkers <123515319+ovswillingworkers@users.noreply.github.com>
2024-03-11 18:58:29 -0700

    Update jobposteditor.tsx. Might need undo

--- app/dashboard/careerpost/jobposteditor.tsx
+++ app/dashboard/careerpost/jobposteditor.tsx
@@ -1,6 +1,6 @@
 //import axios from "axios";
-import { useEffect, useRef, useState } from "react";
-import toast from "react-hot-toast";
+//import { useEffect, useRef, useState } from "react";
+//import toast from "react-hot-toast";
 
 interface JobPosting {
   title: string;
